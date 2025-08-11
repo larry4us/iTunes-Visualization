f8eaa361e132313b4acd40eacd9be5515ad556c1
larry4us <89364723+larry4us@users.noreply.github.com>
2025-08-11 14:43:55 -0400

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Pedro Larry Rodrigues Lopes on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicTests/MusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTests.swift
+//  MusicTests
+//
+//  Created by Pedro Larry Rodrigues Lopes on 11/08/25.
+//
+
+import Testing
+@testable import Music
+
+struct MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
